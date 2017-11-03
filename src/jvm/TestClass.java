ad8ba3c3f8324f9ce5f4fa123e31de8c945eb636
yang <634228743@qq.com>
2017-11-03 10:24:33 +0800

    add bytecode demo

---
+++ src/jvm/TestClass.java
@@ -0,0 +1,15 @@
+package jvm;
+
+/**
+ * Created by CS on 2017/11/2.
+ */
+public class TestClass {
+    private int m;
+    public int inc() {
+        return m+1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println((int)'m');
+    }
+}
